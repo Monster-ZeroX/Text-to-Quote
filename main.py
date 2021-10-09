280c992a2825d2a97405a79f3b1a6b9a35957da3
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-10-09 19:38:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import QueryIdInvalid, FloodWait
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InlineQuery, InlineQueryResultArticle, \
+    InputTextMessageContent
+
+from configs import Config
+
+TorrentBot = Client(session_name=Config.SESSION_NAME, api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
+DEFAULT_SEARCH_MARKUP = [
+                    [InlineKeyboardButton("privacy policy 🍀", url="https://telegra.ph/Quotee-Robot-Privacy-Policy-10-09"),
+                     InlineKeyboardButton("🔎Go Inline", switch_inline_query="!yts ")],
+                    [InlineKeyboardButton("👨‍💻Updates channel ", url="https://t.me/senuinfinity")]
+                ]
+
+
+@Quotee_Robot.on_message(filters.command("start"))
+async def start_handler(_, message: Message):
+    try:
+        await message.reply_sticker("CAACAgUAAxkBAAIDFmFhoElBHoMtp19rgK-wckP3nQXwAAIzBAACqsGQVmP_dWUaSa51HgQ")
+        await message.reply_text(
+            text="😋Hello, Welcome! to @Quotee_Robot. ⁇\n"
+                 "ZQuoteeBot is available to create quotes that can be easily used anywhere.\n\n"
+                 "from here, all the text you provide will be created as a quote. \n\nBy using our service you must agree to our privacy policy",
+            disable_web_page_preview=True,
+            parse_mode="Markdown",
+            reply_markup=InlineKeyboardMarkup(DEFAULT_SEARCH_MARKUP)
+        )
+    except FloodWait as e:
+        print(f"[{Config.SESSION_NAME}] - Sleeping for {e.x}s")
+        await asyncio.sleep(e.x)
+        await start_handler(_, message)
